26b859775d08a3e59f98d31d65f8a7e995adf186
tangqy <121102723@qq.com>
2023-11-30 17:50:50 +0800

    update text2sql_gui.py and relevant func

--- codegen/text2sql/setup.sh
+++ codegen/text2sql/setup.sh
@@ -3,10 +3,10 @@
 ########创建database并导入sql#########
 
 # 创建 RDS MySQL 数据库实例,命名为 mydb,db.t2.micro 类型,用户名 admin,密码为 abc123
-aws rds create-db-instance --db-instance-identifier llm3 --engine mysql --db-instance-class db.r6g.large --master-username admin --master-user-password admin12345678 --allocated-storage 100
+aws rds create-db-instance --db-instance-identifier llm --engine mysql --db-instance-class db.r6g.large --master-username admin --master-user-password admin12345678 --allocated-storage 100
 # 等待数据库实例创建完成并可用
-aws rds wait db-instance-available --db-instance-identifier llm3
-endpoint=$(aws rds describe-db-instances --db-instance-identifier llm2 --query 'DBInstances[*].Endpoint.Address' --output text)
+aws rds wait db-instance-available --db-instance-identifier llm
+endpoint=$(aws rds describe-db-instances --db-instance-identifier llm --query 'DBInstances[*].Endpoint.Address' --output text)
 echo "Endpoint: $endpoint"
 
 # 安装客户端并执行ddl
